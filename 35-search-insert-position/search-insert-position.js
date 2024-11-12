d36cf487d98fabfd307cf6fa3c9bdc399fde883f
local-resident <seotai78@naver.com>
2024-11-12 17:43:57 +0900

    Time: 0 ms (100.00%) | Memory: 48.3 MB (97.06%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let lo = 0, hi = nums.length - 1
+    while (lo <= hi) {
+        let mid = Math.floor((lo + hi) / 2)
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] > target) {
+            hi = mid - 1
+        } else {
+            lo = mid + 1
+        }
+    }
+    return lo
+};
