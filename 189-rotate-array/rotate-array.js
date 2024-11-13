a813bb62f599e6f97f7b6459dfb10804f8591fe9
local-resident <seotai78@naver.com>
2024-11-13 16:41:10 +0900

    Time: 4 ms (31.86%) | Memory: 60.6 MB (44.45%) - LeetSync

--- 189-rotate-array/rotate-array.js
+++ 189-rotate-array/rotate-array.js
@@ -4,7 +4,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    const J = nums.length - (k % nums.length)
-    const arrJ = nums.slice(0, J), arrK = nums.slice(J)
-    nums.splice(0, nums.length, ...arrK, ...arrJ)
+    const reverse = (start, end) => {
+        let l = start, r = end
+        while (l < r) {
+            [nums[l], nums[r]] = [nums[r], nums[l]]
+            l += 1
+            r -= 1
+        }
+        return nums
+    }
+    const m = k % nums.length
+    reverse(0, nums.length - 1)
+    reverse(0, m-1)
+    reverse(m, nums.length - 1)
 };
